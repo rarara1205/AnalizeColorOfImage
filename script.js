95b2f74ffaa2e90bd1bb58afb11ecd16f971afa1
rarara_lab <moriwaki.takara519@mail.kyutech.jp>
2023-07-14 18:16:57 +0900

    [Add] EyedroppedColor

--- script.js
+++ script.js
@@ -84,6 +84,11 @@ let selectMode = document.getElementById("selectMode");
 let canvasImage = document.getElementById("canvasImage");
 input.addEventListener("change", DisplayImage);
 selectMode.addEventListener("change", CreatePoints);
+canvasImage.addEventListener("click", Eyedropper);
+let inputEyeDroppedColor = document.getElementById("inputEyedropedColor");
+let buttonClearScene = document.getElementById("buttonClearScene");
+buttonClearScene.addEventListener("click", ClickClearScene);
+// canvasImage.addEventListener("wheel", Zoom);
 
 let ctx = canvasImage.getContext("2d", {willReadFrequently: true});
 let imageData = null;
@@ -91,9 +96,13 @@ let imageColors = [];
 let imageSize = 0;
 let colorItemSize = 0;
 let image = new Image();
+let particles = null;
 const RADIUS = 200;
 const theta = l => (1-l)*Math.PI;
-const pointSize = 5;
+const pointSize = 3.5;
+let scale = 1;
+let mode = selectMode.value;
+
 
 window.onload = init();
 
@@ -120,6 +129,102 @@ function DisplayImage(){
     }
 }
 
+function rgb2hex(r,g,b){
+    r = r.toString(16);
+    if(r.length == 1) r = "0" + r;
+    g = g.toString(16);
+    if(g.length == 1) g = "0" + g;
+    b = b.toString(16);
+    if(b.length == 1) b = "0" + b;
+    return "#"+r+g+b;
+}
+
+function Eyedropper(e){
+    const [w,h] = [canvasImage.clientWidth, canvasImage.clientHeight];
+    //マウス座標の取得
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
+    //指定座標のImageDataオブジェクトの取得
+    let dx = parseInt(lerp(0,image.width,mouseX/(w-1)));
+    let dy = parseInt(lerp(0,image.height,mouseY/(h-1)));
+    const imageData = ctx.getImageData(dx, dy, 1, 1);
+    let r = imageData.data[0];
+    let g = imageData.data[1];
+    let b = imageData.data[2];
+    inputEyeDroppedColor.value = rgb2hex(r,g,b);
+    [r,g,b] = [r/255,g/255,b/255];
+    
+    let vertices = [r*RADIUS,g*RADIUS,b*RADIUS];
+    if (mode == "HSL"){
+        const [h,s,l] = rgb2hsl(r,g,b);
+        vertices = [s*RADIUS*Math.sin(theta(l))*Math.cos(h),
+            RADIUS*Math.cos(theta(l)),
+            s*RADIUS*Math.sin(theta(l))*Math.sin(h)];
+    }
+
+    const colors = [r,g,b];    
+    const sizes = [100];
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+    geometry.setAttribute('size', new THREE.Float32BufferAttribute(sizes, 1));
+    const pointTexture = new THREE.TextureLoader().load("disc.png");
+    const material = new THREE.ShaderMaterial({
+        uniforms:{
+            pointTexture: {value: pointTexture}
+        },
+        vertexShader: document.getElementById("PointVertexShader").textContent,
+        fragmentShader: document.getElementById("PointFragmentShader").textContent
+    });
+    const mesh = new THREE.Points(geometry, material);
+    scene.add(mesh);
+}
+
+const MAX_SCALE = 5;
+const SCALE_STEP = 0.2;
+let imageScale = 1, imageScaleIndex = 0;
+let mouseX, mouseY;
+let zoomWidth, zoomHeight, zoomLeft=0, zoomTop=0;
+// function Zoom(e){
+//     mouseX = e.offsetX;
+//     mouseY = e.offsetY;
+//     if(e.wheelDelta > 0){
+//         imageScaleIndex++;
+//         imageScale = 1 + imageScaleIndex * SCALE_STEP;
+//         if(imageScale > MAX_SCALE){
+//             imageScale = MAX_SCALE;
+//             imageScaleIndex--;
+//         }
+//         else {
+//             zoomWidth = canvasImage.clientWidth / imageScale;
+//             zoomHeight = canvasImage.clientHeight / imageScale;
+
+//             zoomLeft += mouseX * SCALE_STEP / (imageScale * (imageScale - SCALE_STEP));
+//             zoomLeft = Math.max(0, Math.min(canvasImage.clientWidth - zoomWidth, zoomLeft));
+
+//             zoomTop += mouseY * SCALE_STEP / (imageScale * (imageScale - SCALE_STEP));
+//             zoomTop = Math.max(0, Math.min(canvasImage.clientHeight - zoomHeight, zoomTop));
+//         }
+//     }
+//     else {
+//         imageScaleIndex--;
+//         imageScale = 1 + imageScaleIndex * SCALE_STEP;
+//         if(imageScale < 1){
+//             imageScale = 1;
+//             zoomLeft = 0;
+//             zoomTop = 0;
+//             imageScaleIndex = 0;
+//         }
+//         else{
+//             zoomWidth = canvasImage.clientWidth / imageScale;
+//             zoomHeight = 
+//         }
+//     }
+//     ctx.clearRect(0, 0, e.target.width, e.target.height);
+//     scale += e.deltaY * -0.001;
+//     scale = Math.min(Math.max(0.5, scale), 2)
+// }
+
 function CheckFileExtension(fileName){
   return fileName.split(".").pop();
 }
@@ -131,14 +236,15 @@ function CreatePointsInit(){
     ctx.drawImage(image, 0, 0);
     imageData = ctx.getImageData(0, 0, image.width, image.height);
     imageSize = imageData.data.length;
-    console.log("画素数:"+imageSize);
     colorItemSize = useAlphaChannel ? 4 : 3;
+    if(colorItemSize==4) console.log("画素数:"+imageSize/4);
+    else  console.log("画素数:"+imageSize/3);
     CreatePoints();
 }
 
 function CreatePoints(){
     ClearScene();
-    const mode = selectMode.value;
+    mode = selectMode.value;
     switch (mode){
         case "RGB":
             if(imageData == null){
@@ -153,6 +259,18 @@ function CreatePoints(){
                 break;
             }
             CreateHSLPoints();
+            break;
+    }
+}
+
+function ClickClearScene(){
+    ClearScene();
+    switch (mode){
+        case "RGB":
+            CreateAxisRGB();
+            break;
+        case "HSL":
+            CreateAxisHSL();
             break;
     }
 }
@@ -241,14 +359,15 @@ function CreateRGBPoints(){
 
 function CreateHSLPoints(){
     // const boxes = [];
+    CreateAxisHSL();
     const vertices = [];
     const colors = [];
+    // const RGB = [];
     const sizes = [];
     for(let i=0; i<imageSize; i+=colorItemSize){
         // const geometry = new THREE.BoxGeometry(5, 5, 5);
         let [r, g, b] = [imageData.data[i], imageData.data[i+1], imageData.data[i+2]];
         [r, g, b] = [r/255, g/255, b/255];
-        const [h, s, l] = rgb2hsl(r,g,b);
         // const geometryTrans = geometry.translate(
         //         s*RADIUS*Math.sin(theta(l))*Math.cos(h),
         //         RADIUS*Math.cos(theta(l)),
@@ -262,13 +381,22 @@ function CreateHSLPoints(){
         //     colors[j+2] = b;
         // }
         // geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-        colors.push(r,g,b);
-        vertices.push(
-            s*RADIUS*Math.sin(theta(l))*Math.cos(h),
-            RADIUS*Math.cos(theta(l)),
-            s*RADIUS*Math.sin(theta(l))*Math.sin(h)
-        );
-        sizes.push(pointSize);
+        // const colorIndex = ArraysIncludes(RGB, [r,g,b]);
+        // console.log(colorIndex);
+        // if(colorIndex == -1){        
+            const [h, s, l] = rgb2hsl(r,g,b);
+            // RGB.push([r,g,b]);
+            colors.push(r,g,b);
+            vertices.push(
+                s*RADIUS*Math.sin(theta(l))*Math.cos(h),
+                RADIUS*Math.cos(theta(l)),
+                s*RADIUS*Math.sin(theta(l))*Math.sin(h)
+            );
+            sizes.push(pointSize);
+        // }
+        // else{
+        //     sizes[colorIndex] += 0.1;
+        // }
     }
     // const geometries = BufferGeometryUtils.mergeGeometries(boxes);
     // const material = new THREE.MeshBasicMaterial({
@@ -288,6 +416,44 @@ function CreateHSLPoints(){
         fragmentShader: document.getElementById("PointFragmentShader").textContent
     });
     const mesh = new THREE.Points(geometry, material);
+    scene.add(mesh);
+}
+// h:色相 s:彩度 l:輝度
+function CreateAxisHSL(){
+    const vertices = [];
+    const colors = [];
+    const sizes = [];
+    for(let r=0; r<256; r+=51){
+        for(let g=0; g<256; g+=51){
+            for(let b=0; b<256; b+=51){
+                const [R, G, B] = [r/255, g/255, b/255];
+                let [h, s, l] = rgb2hsl(R,G,B);
+                if(s>=0){
+                    colors.push(R,G,B);
+                    vertices.push(
+                        s*RADIUS*Math.sin(theta(l))*Math.cos(h),
+                        RADIUS*Math.cos(theta(l)),
+                        s*RADIUS*Math.sin(theta(l))*Math.sin(h)
+                    );
+                    sizes.push(20);
+                }
+            }
+        }
+    }
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+    geometry.setAttribute('size', new THREE.Float32BufferAttribute(sizes, 1));
+    const pointTexture = new THREE.TextureLoader().load("disc.png");
+    const material = new THREE.ShaderMaterial({
+        uniforms:{
+            pointTexture: {value: pointTexture}
+        },
+        vertexShader: document.getElementById("PointVertexShader").textContent,
+        fragmentShader: document.getElementById("PointFragmentShader").textContent
+    });
+    const mesh = new THREE.Points(geometry, material);
+
     scene.add(mesh);
 }
 
@@ -369,7 +535,7 @@ function CreateRGBSample(){
 }
 
 function CreateHSLSample(){
-    // let boxes = [];
+    CreateAxisHSL();
     const vertices = [];
     const colors = [];
     const sizes = [];
@@ -422,25 +588,19 @@ function CreateHSLSample(){
         fragmentShader: document.getElementById("PointFragmentShader").textContent
     });
     const mesh = new THREE.Points(geometry, material);
-    scene.add(mesh);
+
     scene.add(mesh);
 }
 
-// function ArrayEqual(a = [], b = []){
-//     return a.toString() == b.toString(); 
-// }
+function ArrayEqual(a = [], b = []){
+    return a.toString() == b.toString(); 
+}
 
-// function ArraysIncludes(arrays = [], array = []){
-//     for(const val of arrays){
-//         if(ArrayEqual(val, array)){
-//             return true;
-//         }
-//     }
-// }
-
-// function RGBIncludes(R=[], G=[], B=[], r, g, b){
-//     if(R.includes(r) && G.includes(g) && B.includes(b)){
-//         return true;
-//     }
-//     return false;
-// }
+function ArraysIncludes(arrays = [], array = []){
+    for(let i=0, len=arrays.length; i<len; i++){
+        if(ArrayEqual(arrays[i], array)){
+            return i;
+        }
+    }
+    return -1;
+}
